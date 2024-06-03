406a9dd219818828906effcf1a058126c1c3566b
Chocolatiines <pimooshwoom@gmail.com>
2024-06-03 15:27:46 +0200

    feat(User): base de User

---
+++ backend/src/User/User.model.ts
@@ -0,0 +1,16 @@
+import { Directive, Field, ID, ObjectType } from '@nestjs/graphql';
+
+@ObjectType({ description: 'User' })
+export class User {
+    @Field(type => ID)
+    id: string;
+
+    @Field()
+    email: string;
+
+    @Field()
+    username: string;
+
+    @Field()
+    password: string;
+}
