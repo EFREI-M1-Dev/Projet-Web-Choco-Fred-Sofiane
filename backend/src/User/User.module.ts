7ca3db40276ab5c7194c89cc09050149798f6029
Sofiane Lasri-Trienpont <alasri250@gmail.com>
2024-06-05 10:16:09 +0200

    Modification du service des messages - Modification de la méthode findOneById pour utiliser prisma - Ajout des méthodes edit et delete

--- backend/src/User/User.module.ts
+++ backend/src/User/User.module.ts
@@ -2,11 +2,11 @@ import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
 import { UserService } from './User.service';
 import { UserResolver } from './User.resolver';
-import {PrismaService} from "../prisma.service";
+import { PrismaService } from '../prisma.service';
 
 @Module({
   imports: [],
-    controllers: [],
-    providers: [UserService, UserResolver, PrismaService],
+  controllers: [],
+  providers: [UserService, UserResolver, PrismaService],
 })
 export class UserModule {}
