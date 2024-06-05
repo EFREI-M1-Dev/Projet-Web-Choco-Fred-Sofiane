0e058966969f9997dce4eb86f67db49d73bf28ec
Fredray-21 <dabadie.frederic@gmail.com>
2024-06-05 10:30:28 +0200

    feat(user) : CRD

--- backend/src/User/User.resolver.ts
+++ backend/src/User/User.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import {Mutation, Query, Resolver} from '@nestjs/graphql';
 import { User } from './User.model';
 import { UserService } from './User.service';
 import {NotFoundException, Param} from '@nestjs/common';
@@ -8,11 +8,22 @@ export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
     @Query(returns => User)
-    async user(@Param('id') id: number): Promise<User> {
+    async findOneById(@Param('id') id: number): Promise<User> {
         const user = await this.userService.findOneById(id);
         if (!user) {
             throw new NotFoundException(id);
         }
         return user;
     }
+
+    @Mutation(returns => User)
+    async createUser(@Param('data') data: User): Promise<User> {
+        return this.userService.createUser(data);
+    }
+
+    @Mutation(returns => User)
+    async deleteUser(@Param('id') id: number): Promise<User> {
+        return this.userService.deleteUser(id);
+    }
+
 }
--- backend/src/User/User.service.ts
+++ backend/src/User/User.service.ts
@@ -1,28 +1,83 @@
-import { Injectable } from '@nestjs/common';
-import { User } from './User.model';
+import {Injectable} from '@nestjs/common';
+import {User} from './User.model';
 import {PrismaService} from "../prisma.service";
+import {Prisma} from "@prisma/client";
+
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {
-  }
-
-  async findOneById(id: number): Promise<User> | null {
-    const user = await this.prisma.user.findUnique({
-        where: {
-            id: id,
-        },
-        });
-
-    if (!user) {
-        return null;
+    constructor(private prisma: PrismaService) {
     }
 
-    return {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-        password: user.password,
-    };
-  }
+    async findOneById(id: number): Promise<User> | null {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: id,
+            },
+        });
+
+        if (!user) {
+            return null;
+        }
+
+        return {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+            password: user.password,
+        };
+    }
+
+    async createUser(data: User): Promise<User> {
+
+        try {
+
+            const user = await this.prisma.user.create({
+                data: {
+                    email: data.email,
+                    username: data.username,
+                    password: data.password,
+                },
+            });
+
+            if (!user) {
+                throw new Error('User not created');
+            }
+
+            return {
+                id: user.id,
+                email: user.email,
+                username: user.username,
+                password: user.password,
+            };
+
+        } catch (error) {
+            if (error instanceof Prisma.PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new Error('Email already exists');
+                }
+            }
+            throw new Error('User not created');
+        }
+    }
+
+    async deleteUser(id: number): Promise<User> {
+        const user = await this.prisma.user.delete({
+            where: {
+                id: id,
+            },
+        });
+
+        if (!user) {
+            throw new Error('User not deleted');
+        }
+
+        return {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+            password: user.password,
+        };
+    }
+
 }
